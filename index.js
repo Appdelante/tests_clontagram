c3126df9de95e79599e3368442db87e0b7640ac4
macadev <danielmacario5@gmail.com>
2020-05-07 08:22:06 -0400

    terminar funcion para automatizar llenado de formulario de signup

--- index.js
+++ index.js
@@ -6,11 +6,26 @@ async function completarSignup() {
   });
   const page = await browser.newPage();
 
-  // Usamos la propiedad 'networkidle0' para esperar a que no hayan requests en vuela por 500 ms
+  // Usamos la propiedad 'networkidle0' para esperar a que no hayan requests en vuelo por 500 ms
   await page.goto('https://beta.clontagram.com/signup', {
     timeout: 15000, // 15 segundos
     waitUntil: 'networkidle0',
   });
+
+  const inputDeEmail = await page.$('form > input[name="email"]');
+  const inputDeNombre = await page.$('form > input[name="nombre"]');
+  const inputDeUsername = await page.$('form > input[name="username"]');
+  const inputDeBio = await page.$('form > input[name="bio"]');
+  const inputDePassword = await page.$('form > input[name="password"]');
+
+  await inputDeEmail.type('prueba88@gmail.com');
+  await inputDeNombre.type('Ana Prueba');
+  await inputDeUsername.type('ana88');
+  await inputDeBio.type('Esta es una cuenta de prueba');
+  await inputDePassword.type('pruebaprueba');
+
+  const botonDeSignup = await page.$('form > button[type="submit"]');
+  await botonDeSignup.click();
 }
 
 completarSignup();
