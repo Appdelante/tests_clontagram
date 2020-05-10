2eb780cb4f99576692b77e74d61ed2b190a7ddc3
macadev <danielmacario5@gmail.com>
2020-05-10 13:11:27 -0400

    empezar a escribir test de signup

--- test/signup.test.js
+++ test/signup.test.js
@@ -1,11 +1,26 @@
-describe('tests de sumar', () => {
-  test('1 + 1 debe dar 2', () => {
-    let x = 1 + 1;
-    expect(x).toEqual(2);
-  });
+const puppeteer = require('puppeteer');
+const { generarUsuario } = require('../generadorDeData');
+const { SIGNUP_URL } = require('../configuracion/urls');
+const PaginaSignup = require('../paginas/paginaSignup');
 
-  test('1 + 2 debe dar 3', () => {
-    let x = 1 + 2;
-    expect(x).toEqual(3);
-  });
+describe('Signup de Clontagram', () => {
+  test('Debe llevar el usuario al feed luego de hacer signup', async () => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
+    await page.goto(SIGNUP_URL, {
+      timeout: 15000, // 15 segundos
+      waitUntil: 'networkidle0',
+    });
+
+    const paginaSignup = new PaginaSignup(page);
+
+    const usuario = generarUsuario();
+    await paginaSignup.llenarFormularioDeSignup(usuario);
+    await paginaSignup.clickSignup();
+
+    // TODO: Verificar que estamos en feed
+
+    await browser.close();
+  }, 15000);
 });
