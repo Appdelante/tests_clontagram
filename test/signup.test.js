47ae59255b01ef6dfd601a46607087c143e3a48f
macadev <danielmacario5@gmail.com>
2020-05-10 12:05:32 -0400

    introduccion a escribir tests con jest

---
+++ test/signup.test.js
@@ -0,0 +1,11 @@
+describe('tests de sumar', () => {
+  test('1 + 1 debe dar 2', () => {
+    let x = 1 + 1;
+    expect(x).toEqual(2);
+  });
+
+  test('1 + 2 debe dar 3', () => {
+    let x = 1 + 2;
+    expect(x).toEqual(3);
+  });
+});
