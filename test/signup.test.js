0d1a03d40fe16e5eca4e12f6366f29e0c321840e
macadev <danielmacario5@gmail.com>
2020-05-12 08:08:43 -0400

    utilizar beforeEach y afterEach en nuestro test de signup

--- test/signup.test.js
+++ test/signup.test.js
@@ -3,24 +3,31 @@ const { generarUsuario } = require('../generadorDeData');
 const { SIGNUP_URL } = require('../configuracion/urls');
 const PaginaSignup = require('../paginas/paginaSignup');
 
+const TIMEOUT_INICIALIZAR_BROWSER = 15000;
+
+let browser, page, paginaSignup;
+beforeEach(async () => {
+  browser = await puppeteer.launch({ headless: false });
+  page = await browser.newPage();
+
+  await page.goto(SIGNUP_URL, {
+    timeout: 15000, // 15 segundos
+    waitUntil: 'networkidle0',
+  });
+
+  paginaSignup = new PaginaSignup(page);
+}, TIMEOUT_INICIALIZAR_BROWSER);
+
+afterEach(async () => {
+  await browser.close();
+});
+
 describe('Signup de Clontagram', () => {
   test('Debe llevar el usuario al feed luego de hacer signup', async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-
-    await page.goto(SIGNUP_URL, {
-      timeout: 15000, // 15 segundos
-      waitUntil: 'networkidle0',
-    });
-
-    const paginaSignup = new PaginaSignup(page);
-
     const usuario = generarUsuario();
     await paginaSignup.llenarFormularioDeSignup(usuario);
     await paginaSignup.clickSignup();
 
     // TODO: Verificar que estamos en feed
-
-    await browser.close();
   }, 15000);
 });
