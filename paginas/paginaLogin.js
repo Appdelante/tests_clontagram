85bd14defd66ecbf010459aad5bab362ce6ce07f
macadev <danielmacario5@gmail.com>
2020-05-13 08:30:41 -0400

    crear modelo para la pagina login

---
+++ paginas/paginaLogin.js
@@ -0,0 +1,23 @@
+const PaginaFeed = require('./paginaFeed');
+
+const SELECTOR_EMAIL = 'form > input[name="email"]';
+const SELECTOR_PASSWORD = 'form > input[name="password"]';
+const SELECTOR_BOTON_LOGIN = 'form > button[type="submit"]';
+
+class PaginaLogin {
+  constructor(page) {
+    super(page);
+  }
+
+  async llenarFormularioDeLogin({ email, password }) {
+    await this.page.type(SELECTOR_EMAIL, email);
+    await this.page.type(SELECTOR_PASSWORD, password);
+  }
+
+  async clickLogin() {
+    await this.page.click(SELECTOR_BOTON_LOGIN);
+    return new PaginaFeed(this.page);
+  }
+}
+
+module.exports = PaginaLogin;
