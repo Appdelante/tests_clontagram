3e700dd44e7153b7cd4eea6787f9134530ca12c9
macadev <danielmacario5@gmail.com>
2020-05-20 08:19:41 -0400

    crear funcion para obtener token de autenticacion del API de clontagram

---
+++ auth/autenticacion.js
@@ -0,0 +1,33 @@
+const axios = require('axios').default;
+const { API_LOGIN_URL } = require('../configuracion/urls');
+
+// Asi se ve una respuesta:
+// {
+//   token:
+//     'eyJhbGciO...',
+//   usuario: {
+//     _id: '5d0304983de857e07ee0d34f',
+//     email: 'holahola@gmail.com',
+//     username: 'jorge',
+//     bio: 'Me gusta la música',
+//     nombre: 'jorge mas',
+//     imagen:
+//       'https://s3.amazonaws.com/vende-tus-corotos-data/imagenes/a5ea9fa6-7ad1-484f-b863-ade8d2d95258.jpg',
+//     siguiendo: false,
+//     numSeguidores: 19,
+//     numSiguiendo: 6,
+//   },
+// };
+module.exports = async function obtenerTokenYDataDeAutenticacion({
+  email,
+  password,
+}) {
+  try {
+    let respuesta = axios.post(API_LOGIN_URL, { email, password });
+    return respuesta.data;
+  } catch (e) {
+    throw new Error(
+      `Usuario con email [${email}] y contraseña [${password}] no pudo ser autenticado. ${e.name}: ${e.message}`
+    );
+  }
+};
--- configuracion/urls.js
+++ configuracion/urls.js
@@ -5,8 +5,11 @@ const LOGIN_URL = `${BASE_URL}/login`;
 
 const POST_EXISTENTE_URL = `${BASE_URL}/post/5ebd4083693b880007e60a36`;
 
+const API_LOGIN_URL = `${BASE_URL}/api/usuarios/login`;
+
 module.exports = {
   SIGNUP_URL,
   LOGIN_URL,
   POST_EXISTENTE_URL,
+  API_LOGIN_URL,
 };
