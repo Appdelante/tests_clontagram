93987a68f3cd2de3cae5d7eb89bf083ccfc47691
macadev <danielmacario5@gmail.com>
2020-05-22 12:18:16 -0400

    escribir test de funcionalidad de dejar comentarios en posts

--- test/post.test.js
+++ test/post.test.js
@@ -3,20 +3,23 @@ const {
   crearPaginaQueRequiereAutenticacion,
 } = require('../paginas/fabricaDePaginas');
 const { POST_EXISTENTE_URL } = require('../configuracion/urls');
-const PaginaLogin = require('../paginas/paginaLogin');
 const PaginaPost = require('../paginas/paginaPost');
 const {
   CREDENCIALES_USUARIO_PARA_TESTS_DE_POSTS,
 } = require('../data/credenciales');
 
 const TIMEOUT_INICIALIZAR_BROWSER = 15000;
+const RUTA_LIKES = '/likes';
+const RUTA_COMENTARIOS = '/comentarios';
 
 let contexto, paginaPost;
 beforeEach(async () => {
   contexto = await crearPaginaQueRequiereAutenticacion({
     url: POST_EXISTENTE_URL,
     credenciales: CREDENCIALES_USUARIO_PARA_TESTS_DE_POSTS,
-    browserConfig: { headless: false },
+    browserConfig: {
+      defaultViewport: { width: 1600, height: 1000 },
+    },
   });
   paginaPost = new PaginaPost(contexto.page);
 }, TIMEOUT_INICIALIZAR_BROWSER);
@@ -29,7 +32,7 @@ describe('Vista Post de Clontagram', () => {
   // Estado: Este test depende de que el post al que naveguemos no tenga un like
   test('Puedo dar y quitar un like', async () => {
     await Promise.all([
-      esperarAQueLlegueRespuestaSobreElLike(),
+      verificarQueLlegaRespuestaDeRuta(RUTA_LIKES),
       paginaPost.clickLike(),
     ]);
 
@@ -37,7 +40,7 @@ describe('Vista Post de Clontagram', () => {
     expect(corazonEstaLleno).toEqual(true);
 
     await Promise.all([
-      esperarAQueLlegueRespuestaSobreElLike(),
+      verificarQueLlegaRespuestaDeRuta(RUTA_LIKES),
       paginaPost.clickLike(),
     ]);
 
@@ -46,14 +49,22 @@ describe('Vista Post de Clontagram', () => {
   }, 20000);
 
   test('Puedo dejar un comentario en un post', async () => {
-    // 1. Navegar al post
-    // 2. Dejar un comentario
-    // 3. Verificar que el comentario aparece
+    const randomString = generarStringRandomizado();
+
+    await Promise.all([
+      verificarQueLlegaRespuestaDeRuta(RUTA_COMENTARIOS),
+      paginaPost.dejarComentario(randomString),
+    ]);
+
+    const textoDelUltimoComentario = await paginaPost.obtenerTextoDelUltimoComentario();
+    expect(textoDelUltimoComentario).toEqual(
+      `${CREDENCIALES_USUARIO_PARA_TESTS_DE_POSTS.username} ${randomString}`
+    );
   });
 });
 
-async function esperarAQueLlegueRespuestaSobreElLike() {
+async function verificarQueLlegaRespuestaDeRuta(ruta) {
   return await contexto.page.waitForResponse((response) => {
-    return response.url().includes('/likes');
+    return response.url().includes(ruta);
   });
 }
